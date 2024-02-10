74fdb83410381e8a73d86d458d7bf85d019df941
Juan Pablo M. <pablochileidh@gmail.com>
2024-02-10 17:01:45 -0300

    inicio captura variables

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+let tareaInput = document.querySelector("#input_tarea")
+let btnAgregaTarea = document.querySelector("#btn_agrega_tarea")
+let listaTareas = document.querySelector("#lista_tareas")
+let datosTareas = document.querySelector("#datos")
