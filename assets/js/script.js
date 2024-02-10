4901801bb2e489066400abdeef893081f499f85b
Juan Pablo M. <pablochileidh@gmail.com>
2024-02-10 17:35:31 -0300

    terminando funcion rendertareas y agregar tareas

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,49 @@ let datosTareas = document.querySelector("#datos")
 
 const tareas = []
 
+
+function renderTareas(){
+    let html = ""
+    headLista = `<table class='table table-responsive'>
+    <tr>
+        <th>
+            Eliminar
+        </th>
+        <th>
+            Realizada
+        </th>
+        <th>
+            ID
+        </th>
+        <th>
+            Nombre
+        </th>
+    </tr>
+        `;
+
+footerLista = `</table>`;
+    for(tarea of tareas){
+        html += `<tr>
+        <td>
+            <button class='btn btn-danger' onclick="borrar(${tarea.id})"> eliminar</button>
+        </td>
+        <td>
+            <button class='btn btn-success' onclick="actualizaEstado(${tarea.id})">Realizada</button>
+            </td>
+        <td>
+            ${tarea.id}
+        </td>
+        <td>
+            ${tarea.nombre}
+        </td>
+    </tr>`;
+    }
+    listaTareas.innerHTML = headLista + html + footerLista
+}
+
 btnAgregaTarea.addEventListener("click",()=>{
     const nuevaTarea = tareaInput.value
     tareas.push({ id: Date.now(), nombre: nuevaTarea, estado: false });
     tareaInput.value = ""
-    let html = ""
-    for(tarea of tareas){
-        html = tarea.nombre
-    }
-    listaTareas.innerHTML += html
+    renderTareas()
 })
