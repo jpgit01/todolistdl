ab5f9b2a4d8c7da2170400d05ec4f8aa1fe51f89
Juan Pablo M. <pablochileidh@gmail.com>
2024-02-10 17:07:18 -0300

    avances

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,9 @@ let tareaInput = document.querySelector("#input_tarea")
 let btnAgregaTarea = document.querySelector("#btn_agrega_tarea")
 let listaTareas = document.querySelector("#lista_tareas")
 let datosTareas = document.querySelector("#datos")
+
+const tareas = []
+
+btnAgregaTarea.addEventListener("click",()=>{
+    console.log("tarea")
+})
